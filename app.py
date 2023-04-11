fff6816ed60e91da7b8794a776044c296b7f45b6
ryzinn18 <ryr.zinn@gmail.com>
2023-04-11 23:23:10 +0000

    Updates for deploy

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
-
+app = Flask(
+    __name__,
+    template_folder="./"
+)
+app.static_folder = "./static/"
 
 @app.route("/")
 def home():
